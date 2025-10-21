74a06aa544edcf9b709565ffdde1a3b28b1eb72b
imaduck25 <fatimaa158@nycstudents.net>
2025-10-21 07:04:07 -0700

    bruh

--- python.py
+++ python.py
@@ -1,7 +1,20 @@
-item1 = [
+item = {
     "name": "apple blossom pastry",
     "price": 6.99,
-    "department": "food & bakery",
     "description": "apple jelly filled baked pastry"
-]
-print(item1["department"])
+}
+print(item["name"])
+
+item = {
+    "name": "pain au chocolat",
+    "price": 5.99,
+    "description": "chocolate creme filled pastry"
+}
+print(item["name"])
+
+item = {
+    "name": "tiramisu",
+    "price": 10.99,
+    "description": "coffee soaked biscuits layered with coffee flavored creme and topped with coffee powder"
+}
+print(item["name"])
