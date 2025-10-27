38058cda970cce02fb66fb8247333b840ab7817a
imaduck25 <fatimaa158@nycstudents.net>
2025-10-27 06:40:48 -0700

    Update python.py

--- python.py
+++ python.py
@@ -1,13 +1,13 @@
 item = {
     "name": "apple blossom pastry",
-    "price": 6.99,
+    "price": 4.99,
     "description": "apple jelly filled baked pastry"
 }
 print(item["name"])
 
 item = {
     "name": "pain au chocolat",
-    "price": 5.99,
+    "price": 4.99,
     "description": "chocolate creme filled pastry"
 }
 print(item["name"])
@@ -18,3 +18,12 @@ item = {
     "description": "coffee soaked biscuits layered with coffee flavored creme and topped with coffee powder"
 }
 print(item["name"])
+
+item = {
+    "name": "macaroon",
+    "price": 7.99,
+    "description": "a delicate meringue-based cookie sandwich filled with a ganache, buttercream or jam"
+}
+print(item["name"])
+
+crepe crossaint
