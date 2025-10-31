38b4ad602d8b6499a1af1e62ef96a7b2f960d2d2
imaduck25 <fatimaa158@nycstudents.net>
2025-10-31 07:14:29 -0700

    Update python.py

--- python.py
+++ python.py
@@ -1,28 +1,35 @@
 
 item = {
     "name": "pain au chocolat",
-    "price": 4.99,
+    "price": 7.55,
     "description": "chocolate creme filled pastry"
 }
 print(item["name"])
 
 item = {
     "name": "tiramisu",
-    "price": 10.99,
+    "price": 7.55,
     "description": "coffee soaked biscuits layered with coffee flavored creme and topped with coffee powder"
 }
 print(item["name"])
 
 item = {
     "name": "macaroon",
-    "price": 7.99,
+    "price": 7.55,
     "description": "a delicate meringue-based cookie sandwich filled with a ganache, buttercream or jam"
 }
 print(item["name"])
 
-cart = []
-print(input("what would you like to buy? "))
-if input == ["pain au chocolat", "tiramisu", "macroon"]:
-    cart.append(item)
-print((item["price"]))
-print("thank you for your purchse")
+
+def store():
+    cart = []
+    print(input("what would you like to buy? "))
+    if input == ["pain au chocolat", "tiramisu", "macroon"]:
+        cart.append(item)
+    print(item["price"])
+    print(input("do you wish to continue? (yes/no)"))
+    if input == "no":
+        print("ty for shopping here")
+    if input == "yes":
+        print(store)
+store()
